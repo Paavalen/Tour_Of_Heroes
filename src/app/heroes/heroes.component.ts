91289009d82b08a79cd6eea9a46e2111a0631f12
Paavalen <p.lingachetti@gmail.com>
2023-07-13 14:03:25 +0400

    style the heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,7 +7,9 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
+
   heroes = HEROES;
   selectedHero?: Hero;
 
