51542e9add8084bfb84db58cf6147d7b3216fcb2
Paavalen <p.lingachetti@gmail.com>
2023-07-14 11:27:19 +0400

    Can delete heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -70,7 +70,7 @@ export class HeroService {
       catchError(this.handleError<any>('updateHero'))
     );
   }
-  
+
   /** POST: add a new hero to the server */
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
@@ -79,6 +79,16 @@ export class HeroService {
     );
   }
 
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+  
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
   this.messageService.add(`HeroService: ${message}`);
